34d04de493734f01f4911c80809e05a22df053e3
sukumar <sukumarg.7310@gmail.com>
2025-07-29 22:35:35 +0530

    terraform vpc module with peering

--- data.tf
+++ data.tf
@@ -7,6 +7,7 @@ data "aws_vpc" "default" {
     default = true
 }
 
+# Terraform data source to get aws default ^ vpc route table id
 data "aws_route_table" "default" {
   vpc_id = data.aws_vpc.default.id
   filter {
